1db0178fd41078d2913e0383f6074ffbf89203f4
Jackson91 <59327633+Jackson91@users.noreply.github.com>
2020-05-31 20:47:24 +0800

    cn.lua

---
+++ cn.lua
@@ -0,0 +1,9 @@
+Config.Locales["cn"] = {
+    ["redemrp_respawn"] ={
+        ["LocaleDead"] = "你死了!",
+        ["LocaleTimer"] = "重生"
+    },
+    ["redemrp_infection"] ={
+        ["msg"] = "你生病了..."
+    }
+}
